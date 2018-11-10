026f5571bd0b60d88567073500efaf98040eeaa1
Rithika <rvaka@eagles.nccu.edu>
2018-11-10 15:25:18 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Lakshmi Vaka on 11/10/18.
+//  Copyright © 2018 Lakshmi Vaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
